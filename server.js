bfdbce2b09440339423d0f52b5fda5249c2376e6
Stefani Georgievska <43820746+sgeorgievska@users.noreply.github.com>
2019-01-21 13:19:26 -0500

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
-
-app.get("/", (reg, res) => {
-    res.send("Stefani Georgievska - 121204150 :)");
-});
-
-app.listen(HTTP_PORT);
